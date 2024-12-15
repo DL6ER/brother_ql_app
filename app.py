cae2fd34ca441ff0654ddb2b5115cfe0fc9c2214
Dodoooh <5958446+Dodoooh@users.noreply.github.com>
2024-12-15 15:14:36 +0100

    fix app_settings.json

--- app.py
+++ app.py
@@ -10,8 +10,7 @@ import logging
 
 app = Flask(__name__)
 
-SETTINGS_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), "settings.json")
-
+SETTINGS_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), "app_settings.json")
 UPLOAD_FOLDER = "uploads"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
